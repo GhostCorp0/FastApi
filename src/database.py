e495566668405f0e1944f40620d22dc918989411
Aman Singh <amansingh08087@gmail.com>
2026-02-19 03:16:14 +0530

    database_url

--- src/database.py
+++ src/database.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import  create_engine
 
 
-DATABASE_URL = "postgresql://fastapi_db_ino9_user:I5r3OIMQGKb6OZePd4xdEcV8ASems9py@dpg-d6aruf6mcj7s73elb61g-a/fastapi_db_ino9"
+DATABASE_URL = "postgresql://user:Download%401234@localhost:5432/fastapi_db"
+
+#DATABASE_URL = "postgresql://fastapi_db_ino9_user:I5r3OIMQGKb6OZePd4xdEcV8ASems9py@dpg-d6aruf6mcj7s73elb61g-a/fastapi_db_ino9"
 
 engine =   create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
