c7298957cf21853b01b78a103d2fd65446020239
Aman Singh <amansingh08087@gmail.com>
2026-02-25 03:08:04 +0530

    code_improvements

--- standard_architecture/src/tasks/controller.py
+++ standard_architecture/src/tasks/controller.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException
-from standard_architecture.src.tasks.dtos import TaskSchema
+from standard_architecture.src.tasks.dtos import TaskSchema,TaskResponseSchema
 from sqlalchemy.orm import Session
 
 from standard_architecture.src.tasks.models import TaskModel
@@ -11,19 +11,19 @@ def create_task(body: TaskSchema, db: Session):
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
-    return {"status": "Task Created Successfully","data":new_task}
+    return new_task
 
 
 def get_tasks(db:Session):
     tasks = db.query(TaskModel).all()
-    return {"status":"All Tasks","data":tasks}
+    return tasks
 
 def get_one_task(task_id:int,db:Session):
     one_task = db.query(TaskModel).get(task_id)
     if not one_task:
         raise HTTPException(status_code=404,detail="Task Id is Incorrect")
 
-    return {"status":"Task Fetched Successfully","data":one_task}
+    return one_task
 
 
 def update_task(body:TaskSchema,task_id:int,db:Session):
@@ -45,7 +45,7 @@ def update_task(body:TaskSchema,task_id:int,db:Session):
     db.commit()
     db.refresh(one_task)
 
-    return {"status":"Task Updated Successfully","data":one_task}
+    return one_task
 
 
 def delete_task(task_id:int,db:Session):
@@ -55,4 +55,4 @@ def delete_task(task_id:int,db:Session):
 
     db.delete(one_task)
     db.commit()
-    return {"status":"Task Deleted Successfully"}
+    return None
